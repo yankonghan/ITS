2a4af7394c76c005302f95421dc03f8e73480d0c
Naruto-Sasuke <ziquanlanyue@163.com>
2015-01-24 12:58:56 +0800

    对Person.cs的代码整理

--- CTTC_ITS_Manage_BackUp/CTTC_ITS_Manage/Person.cs
+++ CTTC_ITS_Manage_BackUp/CTTC_ITS_Manage/Person.cs
@@ -1,28 +1,35 @@
 ﻿using System;
 
-namespace CTTC_ITS_Manage {
-    class Person {
+namespace CTTC_ITS_Manage
+{
+    class Person
+    {
         string firstName;
         string secondName;
         string comments;
-        public Person(string firstName, string secondName) {
+        public Person(string firstName, string secondName)
+        {
             this.firstName = firstName;
             this.secondName = secondName;
             comments = String.Empty;
         }
         public Person(string firstName, string secondName, string comments)
-            : this(firstName, secondName) {
+            : this(firstName, secondName)
+        {
             this.comments = comments;
         }
-        public string FirstName {
+        public string FirstName
+        {
             get { return firstName; }
             set { firstName = value; }
         }
-        public string SecondName {
+        public string SecondName
+        {
             get { return secondName; }
             set { secondName = value; }
         }
-        public string Comments {
+        public string Comments
+        {
             get { return comments; }
             set { comments = value; }
         }
